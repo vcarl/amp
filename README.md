5831e855caa671c22e4e39985d2ea6c3fc27bcbd
Carl Vitullo <carl.vitullo@gmail.com>
2015-01-19 14:36:32 -0500

    Demo improvements

--- README.md
+++ README.md
@@ -17,18 +17,25 @@ Easily create simple straight line animations.
 		// The length in pixels of the line.
 		length: 100,
 		// The angle (in radians) the line should be drawn at.
+		// Math.PI / 2 (default)
 		angle: Math.PI / 2,
-		// The easing function to use. 
-		// Currently the only easing function provided is exponential out.
+		// The easing function to use.
+		// Amp.ease.linear (default)
 		easing: Amp.ease.expOut,
-		// The delay (in seconds) before beginning animation.
-		delay: 1,
-		// The length of time (in seconds) drawing should take.
-		duration: 1,
+		// The delay (in ms) before beginning animation.
+		// 1000 (default)
+		delay: 1000,
+		// The length of time (in ms) drawing should take.
+		// 1000 (default)
+		duration: 1000,
 		// The width of the line in px.
 		width: 5,
-		// Style can be anything context.strokeStyle is compatible with
-		style: "#0000ff"
+		// Style can be anything context.strokeStyle is compatible with.
+		// "#000000" (default)
+		style: "#0000ff",
+		// What kind of cap the line should have.
+		// round, square, butt (default)
+		cap: 'round'
 	});
 
 	// Begin the animations.
--- src/main.js
+++ src/main.js
@@ -1,60 +1,83 @@
 requirejs.config({
-    "paths": {
-      "jquery": "http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
-      "underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min"
-    }
+		"paths": {
+			"jquery": "http://ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min",
+			"underscore": "https://cdnjs.cloudflare.com/ajax/libs/underscore.js/1.7.0/underscore-min"
+		}
 });
 
 require(['amp', 'jquery', 'underscore'], function(Amp, $, _) {
-	var canvas, section, width, height;
-	section = $('#main-content');
+	// Boring size control stuff.
+	var canvas, parent, width, height, lastCount;
+	// Get the size of the parent and set the demo to be the width of the parent
+	// and be 1/3rd as tall as it is wide.
+	parent = $('#demo').parent();
+	width = parent.width();
+	height = parent.width() / 3;
 	canvas = document.getElementById('demo');
-	width = section.width();
-	height = section.width() / 3;
 	canvas.height = height;
-	canvas.width = section.width();
+	canvas.width = parent.width();
 
+	// Initialize a new instance with the canvas, from which a context will be
+	// extracted.
 	var Amp = new Amp(canvas);
 
-	window.count = 10;
-	window.ease = Amp.ease.outExpo;
-
+	// Demo-specific creation function. This lays out a few restrictions on
+	// parameters (colors, direction, etc) and randomly creates lines. The 
+	// lines could also be a pre-created array rather than random.
 	var create = function(n, ease) {
-			var colors = [
+		var tmp;
+		var colors = [
 			"#00e6ff",
 			"#0066ff",
 			"#1900ff",
 			"#ff9900"
 		];
-	  var directions = [
-	     Math.PI / 4,
-	     3 * Math.PI / 4
-	  ];
-	  for (var i = 0; i < n; i++) {
-	  	Amp.addLine({
-				origin: {
-					x: Math.floor(Math.random() * width),
-					y: -5
-				},
-				length: 100 + Math.floor(Math.random() * (height - 70)), 
-				angle: directions[Math.floor(Math.random() * 2)],
-				easing: ease,
-				delay: Math.random(),
-				duration: 1,
-				width: 10 * Math.random(),
-				style: colors[Math.floor(Math.random() * 4)]
-			});
-	  };
+		var directions = [
+			Math.PI / 4,
+			3 * Math.PI / 4,
+			5 * Math.PI / 4,
+			7 * Math.PI / 4
+		];
+		if (lastCount != n) {
+			// If we're drawing a different number of lines, create new random lines.
+			Amp.shapes = [];
+			for (var i = 0; i < n; i++) {
+				Amp.addLine({
+					origin: {
+						x: Math.floor(Math.random() * width),
+						y: Math.floor(Math.random() * height)
+					},
+					length: 30 + Math.floor(Math.random() * (height - 70)), 
+					angle: directions[Math.floor(Math.random() * 4)],
+					easing: ease,
+					delay: Math.random() * 1000,
+					duration: 1000,
+					cap: 'round',
+					width: 10 * Math.random(),
+					style: colors[Math.floor(Math.random() * 4)]
+				});
+			};
+		} else {
+			// If we're drawing the same number of lines, just update the easing
+			// function and reset its state.
+			for (var i = 0; i < n; i++) {
+				Amp.shapes[i].easing = ease;
+				// Amp.shapes[i].done = false;
+			};
+		}
+		lastCount = n;
 	};
-	create(10, Amp.ease.outExpo);
+	create(50, Amp.ease.outExpo);
 	Amp.draw();
 
+	// Boring form related stuff.
 	$(document).ready(function() {
-		var $east, $count;
-		$ease = $('select[name=ease]');
-		$ease.change(function(event) {
+		$('[data-draw]').click(function(event) {
+			var $east, $count;
+			$ease = $('select[name=ease]');
 			$count = $('input[name=count]');
-			create($count.val(), Amp.ease[event.target.value]);
+			create($count.val(), Amp.ease[$ease.val()]);
+			Amp.clear();
 			Amp.draw();
 		});
 	});
